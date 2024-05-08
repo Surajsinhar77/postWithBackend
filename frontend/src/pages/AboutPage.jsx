030250c0e04b836cd31d466ce2075bfb14c9211b
Suraj Kumar <surajsinhaking77@gmail.com>
2024-05-08 19:07:08 +0530

    done some here phire

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,4 +1,4 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const TeamMembers = [
