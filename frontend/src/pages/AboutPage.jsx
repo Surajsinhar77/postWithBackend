84c42016a9af38776e414316f4505b1c583c2e83
Suraj Kumar <surajsinhaking77@gmail.com>
2024-05-08 20:53:12 +0530

    done here some bug

--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -1,11 +1,10 @@
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTwitter, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const TeamMembers = [
   {
     id: 1,
     name: 'Suraj Kumar',
-    imageUrl: 'https://www.pexels.com/photo/woman-in-black-spaghetti-strap-top-holding-makeup-3979134/', 
+    imageUrl: 'https://shorturl.at/nrsNW', 
     socialLinks: {
       linkedin: 'https://www.linkedin.com/in/johndoe',
       twitter: 'https://twitter.com/johndoe',
@@ -15,7 +14,7 @@ const TeamMembers = [
   {
     id: 2,
     name: 'Kauhal Kishor',
-    imageUrl: 'https://www.pexels.com/photo/woman-in-black-spaghetti-strap-top-holding-makeup-3979134/', 
+    imageUrl: 'https://shorturl.at/nrsNW', 
     socialLinks: {
       linkedin: 'https://www.linkedin.com/in/johndoe',
       twitter: 'https://twitter.com/johndoe',
@@ -38,14 +37,16 @@ const AboutPage = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           {TeamMembers.map((member) => (
             <div key={member.id} className="bg-white rounded-lg shadow-lg p-6">
-              <img src={member.imageUrl} alt={member.name} className="h-32 w-32 rounded-full mx-auto mb-4" />
+              <img src={member?.imageUrl} alt={member.name} className="h-32 w-32 rounded-full mx-auto mb-4" />
               <h3 className="text-lg font-semibold mb-2">{member.name}</h3>
               <div className="flex justify-center space-x-4">
                 <a href={member.socialLinks.linkedin} target="_blank" rel="noopener noreferrer">
-                  <i className="fab fa-linkedin text-blue-500 hover:text-blue-700"></i>
+                  <i className="fab fa-linkedin text-blue-500 hover:text-blue-700">
+                    <FaLinkedin className="text-3xl"/>
+                  </i>
                 </a>
                 <a href={member.socialLinks.twitter} target="_blank" rel="noopener noreferrer">
-                  <i className=" fab faTwitter text-blue-500 hover:text-blue-700"></i>
+                  <FaTwitter className="text-3xl"/>
                 </a>
               </div>
             </div>
@@ -53,6 +54,7 @@ const AboutPage = () => {
         </div>
       </div>
     </div>
+
   );
 };
 
